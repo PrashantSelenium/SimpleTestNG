99f793e145e98412d429098b21dc85288e38336b
PrashantSelenium <dhage.prashant@gmail.com>
2016-11-27 13:01:20 +0530

    Initial

---
+++ src/test/java/com/testng/practice/RamboTest.java
@@ -0,0 +1,22 @@
+package com.testng.practice;
+
+import org.testng.annotations.Test;
+
+public class RamboTest {
+
+	@Test
+	public void Test1() {
+		System.out.println("Hello Test1");
+	}
+
+	@Test
+	public void Test2() {
+		System.out.println("Hello Test2");
+	}
+
+	@Test
+	public void Test3() {
+		System.out.println("Hello Test3");
+	}
+
+}
